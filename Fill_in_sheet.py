0a04a089057d3efcff046a1c1dadc44907011459
楊喻翔 <seanyang@yangyuxiangs-MacBook-Pro.local>
2017-07-18 11:09:42 +0800

    if DAU is 0, give 1 instead.

--- Fill_in_sheet.py
+++ Fill_in_sheet.py
@@ -221,6 +221,8 @@ def history_occurrences_slope_calculator(crash_count_list, crash_rate_data):
         print(dau_list)
         temp_crash_rate = []
         for i in range(0, len(crash_count_list), 1):
+            if dau_list[i] == 0:
+                dau_list[i] += 1
             temp_crash_rate.append(float(crash_count_list[i]) / float(dau_list[i].replace(',', '')))
 
         print(temp_crash_rate)
